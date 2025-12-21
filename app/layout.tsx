37654bd167a4bb11450adc2133c8880286fd6c05
anugrahrk <chn23mca024@ceconline.edu>
2025-12-21 19:43:02 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Providers>
           <div className="h-screen">
+          <Analytics/>
             <BackgroundRippleEffect className="no-print"/>
           {children}
           </div>
