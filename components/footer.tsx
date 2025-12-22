01c0ebdd918478b4810395443316283050b61fba
anugrahrk <chn23mca024@ceconline.edu>
2025-12-22 23:35:35 +0530

    added z index to download pdf button

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,6 +3,6 @@ import { Button } from './ui/button'
 
 export const Footer = ({slug}:{slug:string}) => {
   return (
-        <div className='no-print relative bottom-0 left-0 max-w-fit bg-white flex justify-start gap-2 p-2 border border-gray-200 rounded-lg backdrop:backdrop-blur-md'><a href={`/api/pdf?slug=${slug}`}><Button className='cursor-pointer'>download PDF</Button></a></div>
+        <div className='no-print z-100 relative bottom-0 left-0 max-w-fit bg-white flex justify-start gap-2 p-2 border border-gray-200 rounded-lg backdrop:backdrop-blur-md'><a href={`/api/pdf?slug=${slug}`}><Button className='cursor-pointer'>download PDF</Button></a></div>
   )
 }
